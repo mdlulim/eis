aab3cd6b69f8032186c03e5146c7ba91054983e4
Chirag Gondaliya <chirag9151@gmail.com>
2017-12-28 14:24:45 +0530

    Footer Text changes
    Footer Text changes

--- admin/language/en-gb/common/footer.php
+++ admin/language/en-gb/common/footer.php
@@ -1,5 +1,5 @@
 <?php
 // Text
 //$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_footer']  = 'Powered By <a href="http://dashlogic.co.za" target="_blank">Dashlogic</a> &copy; 2014-' . date('Y') . ' All Rights Reserved.';
+$_['text_footer']  = 'Powered By <a href="http://www.cloudlogic.co.za" target="_blank">Cloudlogic</a> &copy; 2014-' . date('Y') . ' All Rights Reserved.';
 $_['text_version'] = 'Version %s';
