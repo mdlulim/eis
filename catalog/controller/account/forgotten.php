1765aa0e53bdc5129e932ca720d582c74da67abd
Thembinkosi <thembinkosi.klein@gmail.com>
2018-11-19 11:07:26 +0200

    Fix: The system displays incorrect time. Update reset password email template on wholesale

--- catalog/controller/account/forgotten.php
+++ catalog/controller/account/forgotten.php
@@ -20,27 +20,30 @@ class ControllerAccountForgotten extends Controller {
 
 			$this->model_account_customer->editCode($this->request->post['email'], $code);
 
-			$subject = sprintf($this->language->get('text_subject'), html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8'));
-
-			$message  = sprintf($this->language->get('text_greeting'), html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8')) . "\n\n";
-			$message .= $this->language->get('text_change') . "\n\n";
-			$message .= $this->url->link('account/reset', 'code=' . $code, true) . "\n\n";
-			$message .= sprintf($this->language->get('text_ip'), $this->request->server['REMOTE_ADDR']) . "\n\n";
-
-			$mail = new Mail();
-			$mail->protocol = $this->config->get('config_mail_protocol');
-			$mail->parameter = $this->config->get('config_mail_parameter');
-			$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
-			$mail->smtp_username = $this->config->get('config_mail_smtp_username');
-			$mail->smtp_password = html_entity_decode($this->config->get('config_mail_smtp_password'), ENT_QUOTES, 'UTF-8');
-			$mail->smtp_port = $this->config->get('config_mail_smtp_port');
-			$mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
+			$subject               = sprintf($this->language->get('text_subject'), html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8'));
+			$data['text_heading']  = $this->language->get('text_heading');
+			$data['text_greeting'] = sprintf($this->language->get('text_greeting'), html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8'));
+			$data['text_change']   = $this->language->get('text_change');
+			$data['reset_link']    = $this->url->link('account/reset', 'code=' . $code, true);
+			$data['text_ip']       = sprintf($this->language->get('text_ip'), $this->request->server['REMOTE_ADDR']);
+			$data['text_notice']   = sprintf($this->language->get('text_notice'), $this->config->get('config_email'), $this->config->get('config_email'));
+			$data['text_footer']   = sprintf($this->language->get('text_footer'), $this->config->get('config_email'), $this->config->get('config_email'));
+			
+			$mail                  = new Mail();
+			$mail->protocol        = $this->config->get('config_mail_protocol');
+			$mail->parameter       = $this->config->get('config_mail_parameter');
+			$mail->smtp_hostname   = $this->config->get('config_mail_smtp_hostname');
+			$mail->smtp_username   = $this->config->get('config_mail_smtp_username');
+			$mail->smtp_password   = html_entity_decode($this->config->get('config_mail_smtp_password'), ENT_QUOTES, 'UTF-8');
+			$mail->smtp_port       = $this->config->get('config_mail_smtp_port');
+			$mail->smtp_timeout    = $this->config->get('config_mail_smtp_timeout');
 
 			$mail->setTo($this->request->post['email']);
 			$mail->setFrom($this->config->get('config_email'));
 			$mail->setSender(html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8'));
 			$mail->setSubject(html_entity_decode($subject, ENT_QUOTES, 'UTF-8'));
-			$mail->setText(html_entity_decode($message, ENT_QUOTES, 'UTF-8'));
+			$mail->setHtml($this->load->view('mail/forgotten', $data));
+			$mail->setText($text);
 			$mail->send();
 
 			$this->session->data['success'] = $this->language->get('text_success');
