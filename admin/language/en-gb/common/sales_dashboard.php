86247710ad541cbcc4ca57d675ead7e00c1682f9
Thembinkosi <thembinkosi.klein@gmail.com>
2018-12-11 16:04:05 +0200

    REPL-544

--- admin/language/en-gb/common/sales_dashboard.php
+++ admin/language/en-gb/common/sales_dashboard.php
@@ -2,5 +2,16 @@
 // Heading
 $_['heading_title'] = 'Sales Management';
 
+// Order Statuses
+$_['order_status_pending_id']    = 1;       # Pending
+$_['order_status_processing_id'] = 2;       # Processing
+$_['order_status_confirmed_id']  = 5;       # Confirmed
+$_['order_status_cancelled_id']  = 7;       # Cancelled
+
+// Quote Statuses
+$_['quote_status_pending_id']    = 1;
+$_['quote_status_converted_id']  = 2;
+$_['quote_status_denied_id']     = 3;
+
 // Error
 $_['error_install'] = 'Warning: Install folder still exists and should be deleted for security reasons!';
