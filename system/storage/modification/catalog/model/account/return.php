645fdafb98381aeed14e98089f19f1e41cb6ff9f
Thembinkosi <thembinkosi.klein@gmail.com>
2019-03-19 15:42:17 +0200

    delete .DS_Store.orig files from system folder,and git folder from admin/controller

---
+++ system/storage/modification/catalog/model/account/return.php
@@ -0,0 +1,185 @@
+<?php
+class ModelAccountReturn extends Model {
+	public function addReturn($data) {
+		$this->db->query("INSERT INTO `" . DB_PREFIX . "return` SET order_id = '" . (int)$data['order_id'] . "', customer_id = '" . (int)$this->customer->getId() . "', firstname = '" . $this->db->escape($data['firstname']) . "', lastname = '" . $this->db->escape($data['lastname']) . "', email = '" . $this->db->escape($data['email']) . "', telephone = '" . $this->db->escape($data['telephone']) . "', product = '" . $this->db->escape($data['product']) . "', model = '" . $this->db->escape($data['model']) . "', quantity = '" . (int)$data['quantity'] . "', opened = '" . (int)$data['opened'] . "', return_reason_id = '" . (int)$data['return_reason_id'] . "', return_status_id = '" . (int)$this->config->get('config_return_status_id') . "', comment = '" . $this->db->escape($data['comment']) . "', date_ordered = '" . $this->db->escape($data['date_ordered']) . "', date_added = NOW(), date_modified = NOW()");
+
+		$return_id = $this->db->getLastId();
+
+		$this->load->model('account/return');
+
+		$return_info = $this->model_account_return->getReturn($return_id);
+
+		if (! $return_info) {
+			return false;
+		}
+
+		$this->load->model('checkout/order');
+
+		$order_info = $this->model_checkout_order->getOrder($return_info['order_id']);
+
+		$this->load->language('mail/return');
+
+		$this->load->model('extension/mail/template');
+
+		$template_data = array(
+			'key' => 'order.return'
+		);
+
+		if ($order_info && $order_info['email'] == $return_info['email']) {
+			$template_data['store_id'] = $order_info['store_id'];
+		}
+
+		$template = $this->model_extension_mail_template->load($template_data);
+
+		if ($order_info && $order_info['email'] == $return_info['email']) {
+			$template->addData($order_info, 'order');
+
+			$template->data['order_date'] = date($this->language->get('date_format_short'), strtotime($order_info['date_added']));
+		}
+
+		$template->addData($return_info);
+
+		if ($order_info['invoice_no']) {
+			$order_info['invoice_no'] = $order_info['invoice_prefix'] . $order_info['invoice_no'];
+		} else {
+			$order_info['invoice_no'] = '';
+		}
+
+		$order_info['date_added'] = date($this->language->get('date_format_short'), strtotime($order_info['date_added']));
+
+		$order_info['total'] = $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value']);
+
+		if ($order_info['payment_address_format']) {
+			$format = $order_info['payment_address_format'];
+		} else {
+			$format = '{firstname} {lastname}' . "\n" . '{company}' . "\n" . '{address_1}' . "\n" . '{address_2}' . "\n" . '{city} {postcode}' . "\n" . '{zone}' . "\n" . '{country}';
+		}
+
+		$find = array(
+			'{firstname}',
+			'{lastname}',
+			'{company}',
+			'{address_1}',
+			'{address_2}',
+			'{city}',
+			'{postcode}',
+			'{zone}',
+			'{zone_code}',
+			'{country}'
+		);
+
+		$replace = array(
+			'firstname' => $order_info['payment_firstname'],
+			'lastname'  => $order_info['payment_lastname'],
+			'company'   => $order_info['payment_company'],
+			'address_1' => $order_info['payment_address_1'],
+			'address_2' => $order_info['payment_address_2'],
+			'city'      => $order_info['payment_city'],
+			'postcode'  => $order_info['payment_postcode'],
+			'zone'      => $order_info['payment_zone'],
+			'zone_code' => $order_info['payment_zone_code'],
+			'country'   => $order_info['payment_country']
+		);
+
+		$order_info['payment_address'] = str_replace(array("\r\n", "\r", "\n"), '<br />', preg_replace(array("/\s\s+/", "/\r\r+/", "/\n\n+/"), '<br />', trim(str_replace($find, $replace, $format))));
+
+		if ($order_info['shipping_address_format']) {
+			$format = $order_info['shipping_address_format'];
+		} else {
+			$format = '{firstname} {lastname}' . "\n" . '{company}' . "\n" . '{address_1}' . "\n" . '{address_2}' . "\n" . '{city} {postcode}' . "\n" . '{zone}' . "\n" . '{country}';
+		}
+
+		$find = array(
+			'{firstname}',
+			'{lastname}',
+			'{company}',
+			'{address_1}',
+			'{address_2}',
+			'{city}',
+			'{postcode}',
+			'{zone}',
+			'{zone_code}',
+			'{country}'
+		);
+
+		$replace = array(
+			'firstname' => $order_info['shipping_firstname'],
+			'lastname'  => $order_info['shipping_lastname'],
+			'company'   => $order_info['shipping_company'],
+			'address_1' => $order_info['shipping_address_1'],
+			'address_2' => $order_info['shipping_address_2'],
+			'city'      => $order_info['shipping_city'],
+			'postcode'  => $order_info['shipping_postcode'],
+			'zone'      => $order_info['shipping_zone'],
+			'zone_code' => $order_info['shipping_zone_code'],
+			'country'   => $order_info['shipping_country']
+		);
+
+		$order_info['shipping_address'] = str_replace(array("\r\n", "\r", "\n"), '<br />', preg_replace(array("/\s\s+/", "/\r\r+/", "/\n\n+/"), '<br />', trim(str_replace($find, $replace, $format))));
+
+		$template->addData($order_info, 'order');
+
+		$template->data['opened'] = $return_info['opened'] ? $this->language->get('text_yes') : $this->language->get('text_no');
+
+		$template->data['return_date'] = date($this->language->get('date_format_short'), strtotime($return_info['date_ordered']));
+
+		$template->data['comment'] = $return_info['comment'] ? nl2br($return_info['comment']) : '';
+
+		$template->data['return_link'] = (defined('HTTP_ADMIN') ? HTTP_ADMIN : HTTPS_SERVER . 'admin/') . 'index.php?route=' . rawurlencode('sale/return/info') . '&return_id=' . $return_id;
+
+		$template->build();
+
+		$mail = new Mail();
+		$mail->protocol = $this->config->get('config_mail_protocol');
+		$mail->parameter = $this->config->get('config_mail_parameter');
+		$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
+		$mail->smtp_username = $this->config->get('config_mail_smtp_username');
+		$mail->smtp_password = html_entity_decode($this->config->get('config_mail_smtp_password'), ENT_QUOTES, 'UTF-8');
+		$mail->smtp_port = $this->config->get('config_mail_smtp_port');
+		$mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
+
+		$mail->setTo($template->data['store_email']);
+		$mail->setFrom($return_info['email']);
+		$mail->setSender($return_info['firstname'] . ' ' . $return_info['lastname']);
+
+		$template->hook($mail);
+
+		$mail->send();
+
+		$this->model_extension_mail_template->sent();
+
+		return $return_id;
+	}
+
+	public function getReturn($return_id) {
+		$query = $this->db->query("SELECT r.return_id, r.order_id, r.firstname, r.lastname, r.email, r.telephone, r.product, r.model, r.quantity, r.opened, (SELECT rr.name FROM " . DB_PREFIX . "return_reason rr WHERE rr.return_reason_id = r.return_reason_id AND rr.language_id = '" . (int)$this->config->get('config_language_id') . "') AS reason, (SELECT ra.name FROM " . DB_PREFIX . "return_action ra WHERE ra.return_action_id = r.return_action_id AND ra.language_id = '" . (int)$this->config->get('config_language_id') . "') AS action, (SELECT rs.name FROM " . DB_PREFIX . "return_status rs WHERE rs.return_status_id = r.return_status_id AND rs.language_id = '" . (int)$this->config->get('config_language_id') . "') AS status, r.comment, r.date_ordered, r.date_added, r.date_modified FROM `" . DB_PREFIX . "return` r WHERE r.return_id = '" . (int)$return_id . "' AND r.customer_id = '" . $this->customer->getId() . "'");
+
+		return $query->row;
+	}
+
+	public function getReturns($start = 0, $limit = 20) {
+		if ($start < 0) {
+			$start = 0;
+		}
+
+		if ($limit < 1) {
+			$limit = 20;
+		}
+
+		$query = $this->db->query("SELECT r.return_id, r.order_id, r.firstname, r.lastname, rs.name as status, r.date_added FROM `" . DB_PREFIX . "return` r LEFT JOIN " . DB_PREFIX . "return_status rs ON (r.return_status_id = rs.return_status_id) WHERE r.customer_id = '" . $this->customer->getId() . "' AND rs.language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY r.return_id DESC LIMIT " . (int)$start . "," . (int)$limit);
+
+		return $query->rows;
+	}
+
+	public function getTotalReturns() {
+		$query = $this->db->query("SELECT COUNT(*) AS total FROM `" . DB_PREFIX . "return`WHERE customer_id = '" . $this->customer->getId() . "'");
+
+		return $query->row['total'];
+	}
+
+	public function getReturnHistories($return_id) {
+		$query = $this->db->query("SELECT rh.date_added, rs.name AS status, rh.comment FROM " . DB_PREFIX . "return_history rh LEFT JOIN " . DB_PREFIX . "return_status rs ON rh.return_status_id = rs.return_status_id WHERE rh.return_id = '" . (int)$return_id . "' AND rs.language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY rh.date_added ASC");
+
+		return $query->rows;
+	}
+}
